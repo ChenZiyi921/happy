351992092d9c46531e0ee9be1517524adf6f08a0
陈子义 <18310423707@163.com>
2024-06-10 12:27:19 +0800

    部门

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -10,12 +10,16 @@ const defaultProps = {
 }
 
 const data = ref([])
+const cascadeLoading = ref(true)
 const multipleSelection = ref([])
-const currentPage = ref(4)
-const pageSize = ref(100)
+
 const user = reactive({
   list: [],
-  pageinfo: {}
+  pageinfo: {
+    page: 1,
+    records: 368,
+    size: 10
+  }
 })
 
 onMounted(() => {
@@ -23,6 +27,7 @@ onMounted(() => {
 })
 
 departmentCascade({ only_department: 1 }).then((res) => {
+  cascadeLoading.value = false
   if (res.code_status === 0) {
     data.value = res.data.list
   } else {
@@ -30,8 +35,8 @@ departmentCascade({ only_department: 1 }).then((res) => {
   }
 })
 
-const getDepartmentUserList = (data) => {
-  departmentUserList({ department_id: data?.id || "", keywords: "", page: 1, size: 10 }).then((res) => {
+const getDepartmentUserList = ({ id = "", keywords = "", page = 1, size = 10 } = {}) => {
+  departmentUserList({ department_id: id, keywords, page, size }).then((res) => {
     user.list = res.data.list
     user.pageinfo = res.data.pageinfo
   })
@@ -41,18 +46,18 @@ const handleSelectionChange = (val) => {
   multipleSelection.value = val
 }
 
-const handleSizeChange = (val) => {
-  console.log(`${val} items per page`)
+const handleSizeChange = (size) => {
+  getDepartmentUserList({ size })
 }
 
-const handleCurrentChange = (val) => {
-  console.log(`current page: ${val}`)
+const handleCurrentChange = (page) => {
+  getDepartmentUserList({ page })
 }
 </script>
 
 <template>
   <div class="organization">
-    <div style="display: flex; flex-direction: column; background-color: #fff; padding: 20px 0">
+    <div style="display: flex; flex-direction: column; width: 300px; background-color: #fff; padding: 20px 0">
       <el-input
         v-model="input"
         style="
@@ -66,7 +71,13 @@ const handleCurrentChange = (val) => {
         "
         placeholder="搜索部门、员工"
       />
-      <el-tree class="department" :data="data" :props="defaultProps" @node-click="getDepartmentUserList">
+      <el-tree
+        class="department"
+        :data="data"
+        :props="defaultProps"
+        v-loading="cascadeLoading"
+        @node-click="getDepartmentUserList"
+      >
         <template #default="{ node, data }">
           <span class="custom-tree-node">
             <img src="@/assets/folder.png" style="padding-right: 4px" />
@@ -75,44 +86,45 @@ const handleCurrentChange = (val) => {
         </template>
       </el-tree>
     </div>
-    <!-- <div> -->
-    <el-table
-      :data="user.list"
-      style="width: 100%; margin-left: 14px"
-      @selection-change="handleSelectionChange"
-      :header-cell-style="{ backgroundColor: '#f7f7f7' }"
-    >
-      <el-table-column type="selection" width="55" />
-      <el-table-column prop="username" label="姓名" min-width="100" />
-      <el-table-column prop="position" label="职务" min-width="140" />
-      <el-table-column prop="job_name" label="角色" min-width="180" />
-      <el-table-column prop="department_name" label="部门" min-width="140" />
-      <el-table-column prop="account" label="账号" min-width="120" />
-      <el-table-column prop="status" label="状态" min-width="80" />
-      <el-table-column prop="address" label="操作" min-width="180" fixed="right">
-        <template #default="{ row }">
-          <span v-if="row.status == 1">停用</span>
-          <span v-if="row.status == 2">启用</span>
-          <span>重置密码</span>
-          <span v-if="row.status == 2">删除</span>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- <el-pagination
-      v-model:current-page="currentPage"
-      v-model:page-size="pageSize"
-      :page-sizes="[100, 200, 300, 400]"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="400"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    /> -->
-    <!-- </div> -->
+    <div style="width: calc(100% - 314px); margin-left: 14px">
+      <el-table
+        :data="user.list"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+        :header-cell-style="{ backgroundColor: '#f7f7f7' }"
+      >
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="username" label="姓名" min-width="100" />
+        <el-table-column prop="position" label="职务" min-width="140" />
+        <el-table-column prop="job_name" label="角色" min-width="180" />
+        <el-table-column prop="department_name" label="部门" min-width="140" />
+        <el-table-column prop="account" label="账号" min-width="120" />
+        <el-table-column prop="status" label="状态" min-width="80" />
+        <el-table-column prop="address" label="操作" min-width="230" fixed="right">
+          <template #default="{ row }">
+            <span class="operation" v-if="row.status == 1">停用</span>
+            <span class="operation" v-if="row.status == 2">启用</span>
+            <span class="operation">重置密码</span>
+            <span class="operation" v-if="row.status == 2">删除</span>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        v-model:current-page="user.pageinfo.page"
+        v-model:page-size="user.pageinfo.size"
+        :page-sizes="[10, 20, 30, 40]"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="user.pageinfo.records"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 <style lang="less" scoped>
 .organization {
   display: flex;
+  max-width: 100%;
   height: 100%;
   background-color: #f2f3f5;
   .department {
