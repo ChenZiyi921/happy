88751417821ca83022f7fceb80b07a888cc9372f
陈子义 <18310423707@163.com>
2024-06-10 22:37:16 +0800

    del

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -1,6 +1,6 @@
 import { useUserStoreHook } from "@/store/modules/user"
 
-export const checkPermission = (permissionRoles: string[]): boolean => {
+export const checkPermission = (permissionRoles) => {
   if (Array.isArray(permissionRoles) && permissionRoles.length > 0) {
     const { roles } = useUserStoreHook()
     return roles.some((role) => permissionRoles.includes(role))
