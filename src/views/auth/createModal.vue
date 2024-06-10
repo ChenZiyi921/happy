9c8607e43742e1cdf17cfdec52e952d25effe7c0
陈子义 <18310423707@163.com>
2024-06-10 16:03:30 +0800

    调整样式

--- src/views/auth/createModal.vue
+++ src/views/auth/createModal.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, ref } from "vue"
 
+const title = ref("")
 const dialogTableVisible = ref(false)
 const dialogFormVisible = ref(false)
 const formLabelWidth = "140px"
@@ -24,7 +25,7 @@ defineExpose({ setVisible })
 </script>
 
 <template>
-  <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
+  <el-dialog v-model="dialogFormVisible" :title="`${title}权限`" width="500">
     <el-form :model="form">
       <el-form-item label="Promotion name" :label-width="formLabelWidth">
         <el-input v-model="form.name" autocomplete="off" />
@@ -38,8 +39,8 @@ defineExpose({ setVisible })
     </el-form>
     <template #footer>
       <div class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false"> Confirm </el-button>
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button color="#ff7e16" style="color: #fff" @click="dialogFormVisible = false">确定</el-button>
       </div>
     </template>
   </el-dialog>
